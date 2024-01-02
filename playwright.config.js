7c3fd1aea2aa8a32b56e3d7b08ba06044816093d
gaddammahesh21 <126861240+gaddammahesh21@users.noreply.github.com>
2024-01-02 17:35:53 +0530

    updated yml file

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
@@ -65,10 +66,10 @@ export default defineConfig({
       name: 'Microsoft Edge',
       use: { ...devices['Desktop Edge'], channel: 'msedge' },
     },
-    //  {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+     {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -23,16 +23,19 @@
 // });
 import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
+    await page.waitForTimeout(5000); 
     await page.goto('https://edulence.klstaging.net/');
+    await page.waitForTimeout(1000);
     await page.locator('#email').click();
     await page.locator('#email').fill('mahesh.g@elblearning.com');
     await page.locator('#password').click();
     await page.locator('#password').fill('slacker');
     await page.getByRole('button', { name: 'Sign in' }).click();
     await page.goto('https://edulence.klstaging.net/dashboard/featured');
-    await page.waitForTimeout(5000); 
+    await page.waitForTimeout(2000); 
     await page.getByRole('button', { name: 'Accept', exact: true }).click();
     await page.getByText('Enterprise A.').click();
     await page.getByRole('link', { name: 'Admin page' }).click();
-    await page.getByRole('button', { name: 'New ReportsBETA' }).click();
-  });
+    await page.waitForTimeout(3000);
+    page.getByRole('button', { name: 'New ReportsBETA' });
+});
